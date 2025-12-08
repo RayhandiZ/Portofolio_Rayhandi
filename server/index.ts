ff634064801f3fa31bc95992e8b046f98699c079
Himsi <himsistudent@gmail.com>
2025-12-08 12:49:06 +0700

    Telah Berhasil membuat localhost

--- server/index.ts
+++ server/index.ts
@@ -81,18 +81,11 @@ app.use((req, res, next) => {
   }
 
   // ALWAYS serve the app on the port specified in the environment variable PORT
-  // Other ports are firewalled. Default to 5000 if not specified.
-  // this serves both the API and the client.
-  // It is the only port that is not firewalled.
+  // Default to 5000 if not specified.
   const port = parseInt(process.env.PORT || "5000", 10);
-  httpServer.listen(
-    {
-      port,
-      host: "0.0.0.0",
-      reusePort: true,
-    },
-    () => {
-      log(`serving on port ${port}`);
-    },
-  );
+  
+  // PERBAIKAN DI SINI: Menggunakan 127.0.0.1 dan menghapus opsi reusePort
+  httpServer.listen(port, "127.0.0.1", () => {
+    log(`serving on port ${port}`);
+  });
 })();
